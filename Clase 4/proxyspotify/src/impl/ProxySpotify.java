ba9f6c3341e240916cdc0f78bbd1e4b2a8e990ae
veronicajujuy <veronica.jujuy@gmail.com>
2024-07-25 23:05:34 -0300

    agregando clase 4

---
+++ Clase 4/proxyspotify/src/impl/ProxySpotify.java
@@ -0,0 +1,21 @@
+package impl;
+
+import model.Usuario;
+
+public class ProxySpotify implements ServicioSpotify{
+    private Spotify spotify;
+
+    public ProxySpotify() {
+        spotify = new Spotify();
+    }
+
+    @Override
+    public String descargar(Usuario usuario) {
+        if(usuario.getTipo().equals(TipoUsuario.PREMIUM)){
+            return spotify.descargar(usuario);
+        } else {
+
+            return "El usuario debe ser premium para descargar";
+        }
+    }
+}
